60de7ae1544d2e23f691db53ad7d028eced89896
Plywood Wolf <plywoodwolf@gmail.com>
2019-11-05 19:43:51 +0300

    Added linkList class with filter visited ability

--- TtDl.py
+++ TtDl.py
@@ -13,6 +13,21 @@ visited = {}
 files = {}
 startUrl = 'https://thetrove.net/index.html'
 
+class linkList:
+    def __init__(self):
+        dprint('creating linkList obj')
+        self.visited = {}
+        dprint('visited = ', self.visited)
+
+    def filter(self, lst):
+        return [lnk for lnk in lst if lnk not in self.visited]
+
+    def add(self, url):
+        dprint('adding', url, 'to visited')
+        self.visited.add(url)
+        dprint('added', url, 'to visited')
+
+
 
 def dprint(*line):
     global debug
@@ -22,20 +37,20 @@ def dprint(*line):
 
 def parse_links(resp, url):
     soup = bS(resp.text, 'html.parser')
-    dprint(resp.headers)
-    dprint(resp.headers)
     return [urljoin(url, td.find('a').get('href')) for td in soup.find_all('tr', class_='litem dir')]
     # [await urls.put(nurl) for nurl in new_urls if nurl not in visited]
 
-async def crawl(name, urls):
+async def crawl(name, urls, visited):
     global files
     url = await urls.get()
     resp = rq.get(url)
     dprint(resp.headers)
-    [await urls.put(i) for i in parse_links(resp, url)]
+    visited.add(url)
+    dprint('!')
+    [await urls.add(k) for k in visited.filter(parse_links(resp, url))]
     dprint(name)
     # files |= {urljoin(url, td.find('a').get('href')) for td in soup.find_all('tr', class_='litem file')}
-    # dprint(urls.qsize())
+    dprint(urls.qsize())
     #urls.task_done()
 
 
@@ -45,11 +60,12 @@ async def main(url):
     # visited = m.Queue()
     # files = m.Queue()
     urls = asyncio.Queue()
+    visited = linkList()
     await urls.put(url)
 
     tasks = []
     for i in range(10):
-        task = asyncio.create_task(crawl(f'worker-{i}', urls))
+        task = asyncio.create_task(crawl(f'worker-{i}', urls, visited))
         tasks.append(task)
 
     await urls.join()
@@ -66,8 +82,8 @@ async def main(url):
     # pool.join()
 
 if __name__ == '__main__':
-    # asyncio.run(main(startUrl))
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(startUrl))
+    asyncio.run(main(startUrl))
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main(startUrl))
 
 # crawl(startUrl)
